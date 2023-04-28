4a050a9c8721e96ccc6ea8c726e4e2239ac28c90
ilray88 <ilraygo@gmail.com>
2023-04-29 01:18:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-API_KEY = os.environ.get("OPENAI_API_KEY")
+API_KEY = os.environ.get(OPENAI_API_KEY)
 print(API_KEY)
 print("1")
 # [step 1]>> 例如： API_KEY = "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" （此key无效）
